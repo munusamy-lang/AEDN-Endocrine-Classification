c726fd3de44d52ce85b80257a345488a69fd34e1
munusamy-lang <kb6672467@gmail.com>
2026-06-19 13:05:19 +0530

    Create metrics.py

---
+++ Evaluation/metrics.py
@@ -0,0 +1,49 @@
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score
+)
+
+def evaluate_model(
+        y_true,
+        y_pred,
+        y_prob):
+
+    acc = accuracy_score(
+        y_true,
+        y_pred
+    )
+
+    precision = precision_score(
+        y_true,
+        y_pred,
+        average='weighted'
+    )
+
+    recall = recall_score(
+        y_true,
+        y_pred,
+        average='weighted'
+    )
+
+    f1 = f1_score(
+        y_true,
+        y_pred,
+        average='weighted'
+    )
+
+    auc = roc_auc_score(
+        y_true,
+        y_prob,
+        multi_class='ovr'
+    )
+
+    return {
+        "Accuracy":acc,
+        "Precision":precision,
+        "Recall":recall,
+        "F1":f1,
+        "AUROC":auc
+    }
